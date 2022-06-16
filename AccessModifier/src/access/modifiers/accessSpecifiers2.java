a44b3883fde6596b1069daec72f40d79c7340044
Alexander M <contactimedici@gmail.com>
2022-06-16 07:40:19 -0400

    Add files via upload
    AccessModifiers1.4

---
+++ AccessModifier/src/access/modifiers/accessSpecifiers2.java
@@ -0,0 +1,9 @@
+package access.modifiers;
+
+public class accessSpecifiers2 {
+	public static void main(String[] args) {
+		System.out.println("You are using private access specifier"); 
+		priaccessspecifier obj = new priaccessspecifier(); 
+	}
+
+}
